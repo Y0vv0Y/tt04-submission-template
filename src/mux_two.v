62f6ce48a97b4fe231a6531214f2ba6ce620a43e
Y0vv0Y <90146870+Y0vv0Y@users.noreply.github.com>
2023-09-05 21:33:46 +0800

    Create mux_two.v

---
+++ src/mux_two.v
@@ -0,0 +1,11 @@
+module mux_two(out, a, b, sl);
+    input a, b, sl;
+    output out;
+    wire nsl, sela, selb;
+        assign nsl = ~sl;
+        assign sela = a & nsl;
+        assign selb = b & sl;
+        assign out = sela | selb;	
+endmodule
+
+
