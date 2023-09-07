058ff84a21d9485baddf8228b74aa608eec2d0f1
Y0vv0Y <90146870+Y0vv0Y@users.noreply.github.com>
2023-09-07 15:31:32 +0800

    Update mux_two.v

--- src/mux_two.v
+++ src/mux_two.v
@@ -1,14 +1,14 @@
-module tt_um_mux_two(out, ui_in, uio_in, uio_oe, uio_out, uo_out, clk, ena, rst_n);
+module tt_um_mux_two(ui_in, uio_in, uio_oe, uio_out, uo_out, clk, ena, rst_n);
     input clk,ena,rst_n;
     input [7:0] ui_in, uio_in;
     output [7:0] uio_oe, uio_out;
     output [7:0] uo_out;
     //input a, b, sl;
-    output out;
+    // output out;
     
     assign uio_oe = 8'b0000_0000;
     assign uio_out = 8'b0000_0000;
-    // assign uo_out = 8'b0000_0000;
+    assign uo_out = 8'b0000_0000;
     
     wire nsl, sela, selb;
     assign nsl = ~ui_in[1];
